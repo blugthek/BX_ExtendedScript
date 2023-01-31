33b3dca88bb0343d4d4ad3d39cf09cd8549b6c6a
BLUGTHEK <blugthek@gmail.com>
2023-01-31 15:36:41 +0700

    Initial commit : AFK Job

---
+++ BX_AFKJob/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+version '1.0a'
+author 'BLUGTHEK'
+description 'Easy AFK Job By BLUGTHEK'
+
+client_script {
+    'client.lua',
+    'config.lua',
+}
+
+server_scripts {
+    'server.lua',
+    'config.lua',
+}
