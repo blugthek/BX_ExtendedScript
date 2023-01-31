f46313d95c9226ab5f63325267afae62325da66d
BLUGTHEK <blugthek@gmail.com>
2023-01-31 15:39:22 +0700

    Commit : Bossfight still working on it

---
+++ BX_Bossfight/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+version '0.1a'
+author 'BLUGTHEK'
+description 'BossFight By BLUGTHEK'
+
+client_script {
+    'client.lua',
+    'config.lua',}
+
+server_scripts {
+    'server.lua',
+    'config.lua',}
